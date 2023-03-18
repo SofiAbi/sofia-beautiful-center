7a49e94b6571452202f2929e2e49a038cb5eebf8
SofiAbi <128260621+SofiAbi@users.noreply.github.com>
2023-03-18 17:02:01 -0500

    primer codigo

--- src/main.ts
+++ src/main.ts
@@ -12,10 +12,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
     <a target="_blank" href="https://angular.io/start">
       Esta es mi primera aplicación en Angular 
     </a>
+    <img src= {{img}}   width="210" height="240" alt="Logo HTML5">
   `,
 })
 export class App {
-  name = 'Sofia';
+  name = 'Sofia'; 
+  img='https://firebasestorage.googleapis.com/v0/b/sofia-beautiful-center.appspot.com/o/beautiful-center%2Fbeautiful.png?alt=media&token=8b4e9248-f469-4e39-8983-3955dfc10b73';
 }
 
 bootstrapApplication(App);
